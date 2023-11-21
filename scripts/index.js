185591adf2f987555da01168ff840fd7bc2b7feb
mkalandaris <mkalandaris@gmail.com>
2023-11-22 02:49:55 +0400

    crated functions for adding cards and removing them in index js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const places = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(card, callBackFunction) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener('click', function () {
+        callBackFunction(cardElement);
+    });
+
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(elem) {
+    elem.remove();
+};
 
 // @todo: Вывести карточки на страницу
+const len = initialCards.length;
+
+for (let i = 0; i < len; i++) {
+    const card = createCard(initialCards[i], deleteCard);
+    places.append(card);
+};
