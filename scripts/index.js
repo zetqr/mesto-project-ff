deef7c7a3f4d16daded94eea0277d213db14e7c6
mkalandaris <mkalandaris@gmail.com>
2023-11-22 11:21:52 +0400

    small style changes

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@ const cardTemplate = document.querySelector('#card-template').content;
 // выбрать элемент, в ктором будут находиться карточки
 const places = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
 function createCard(card, deleteCardFunction) {
 
-    // @todo: DOM узлы
+    // DOM узлы
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    
+
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__image').alt = card.name;
@@ -23,12 +23,12 @@ function createCard(card, deleteCardFunction) {
     return cardElement;
 };
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
 function deleteCard(elem) {
     elem.remove();
 };
 
-// @todo: Вывести карточки на страницу
+// Вывод карточки на страницу
 initialCards.forEach((cardElement) => {
     const card = createCard(cardElement, deleteCard);
     places.append(card);
