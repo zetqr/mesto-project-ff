c9b7637b64368d79255f4c3024dfb886107a6c18
mkalandaris <mkalandaris@gmail.com>
2023-11-22 10:59:23 +0400

    fixed naming and removed code violation dry

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
-// @todo: Темплейт карточки
+// выбрать темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// выбрать элемент, в ктором будут находиться карточки
 const places = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(card, callBackFunction) {
+function createCard(card, deleteCardFunction) {
 
-    const cardTemplate = document.querySelector('#card-template').content;
+    // @todo: DOM узлы
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
     deleteButton.addEventListener('click', function () {
-        callBackFunction(cardElement);
+        deleteCardFunction(cardElement);
     });
 
     return cardElement;
@@ -28,8 +29,6 @@ function deleteCard(elem) {
 };
 
 // @todo: Вывести карточки на страницу
-const len = initialCards.length;
-
 initialCards.forEach((cardElement) => {
     const card = createCard(cardElement, deleteCard);
     places.append(card);
